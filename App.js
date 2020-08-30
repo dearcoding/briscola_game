a89eb69a6495d344bbf173b3865b0a4f1edf316c
dearcoding <giovanniocchiuto3@gmail.com>
2020-08-30 19:18:11 +0200

    Revert "Revert "lol""
    This reverts commit c46496e760d321cf7a3cbda9bacd7bdbfcfc9265.

---
+++ App.js
@@ -0,0 +1,28 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import createStackNavigator from "@react-navigation/stack/src/navigators/createStackNavigator";
+
+import HomeScreen from "./src/pages/HomeScreen";
+import SelectGameType from "./src/pages/SelectGameType";
+
+const Stack = createStackNavigator();
+
+const App = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="Home"
+                    component={HomeScreen}
+                />
+                <Stack.Screen
+                    name="SelectGame"
+                    component={SelectGameType}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default App;
